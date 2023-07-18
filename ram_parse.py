7dfb71603511b0446b1b899329b521b70de2f376
OriekhovKyrylo <kr.oriekhov@gmail.com>
2023-07-18 20:26:40 +0300

    Initial commit

--- ram_parse.py
+++ ram_parse.py
@@ -3,6 +3,105 @@ import requests
 from config import host, user, password, db, port
 
 
+def create_characters_table(cursor):
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS characters(
+            id SERIAL PRIMARY KEY,
+            serial_number INTEGER UNIQUE,
+            name VARCHAR(100) ,
+            status VARCHAR(100),
+            species VARCHAR(100),
+            gender VARCHAR(100),
+            episodes INTEGER[]
+        );"""
+    )
+
+
+def create_locations_table(cursor):
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS locations(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) UNIQUE,
+            type VARCHAR(100),
+            dimension VARCHAR(100)
+        );"""
+    )
+
+
+def create_episodes_table(cursor):
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS episodes (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(100) UNIQUE,
+            air_date DATE,
+            episode_code VARCHAR(100),
+            character_id INTEGER[]
+        );"""
+    )
+
+
+def insert_characters(cursor, character_data):
+    for character in character_data:
+        serial_number = character['id']
+        name = character['name']
+        status = character['status']
+        species = character['species']
+        gender = character['gender']
+        episodes = [int(episode.split("/")[-1]) for episode in character["episode"]]
+
+        cursor.execute("""
+            INSERT INTO characters (serial_number, name, status, species, gender, episodes)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            ON CONFLICT (serial_number) DO UPDATE
+            SET name = EXCLUDED.name,
+                status = EXCLUDED.status,
+                species = EXCLUDED.species,
+                gender = EXCLUDED.gender,
+                episodes = EXCLUDED.episodes;
+        """, (serial_number, name, status, species, gender, episodes))
+
+
+def insert_locations(cursor, location_data):
+    for location in location_data:
+        name = location['name']
+        type = location['type']
+        dimension = location['dimension']
+
+        cursor.execute("""
+            INSERT INTO locations (name, type, dimension) 
+            VALUES (%s, %s, %s) 
+            ON CONFLICT (name) DO UPDATE 
+            SET type = EXCLUDED.type, dimension = EXCLUDED.dimension
+        """, (name, type, dimension))
+
+
+def insert_episodes(cursor, episodes_data):
+    for episode in episodes_data:
+        name = episode['name']
+        air_date = episode['air_date']
+        episode_code = episode['episode']
+        character_id = [int(character.split("/")[-1]) for character in episode["characters"]]
+
+        cursor.execute("""
+            INSERT INTO episodes (name, air_date, episode_code, character_id)
+            VALUES (%s, %s, %s, %s)
+            ON CONFLICT (name) DO UPDATE
+            SET air_date = EXCLUDED.air_date, episode_code = EXCLUDED.episode_code, character_id = EXCLUDED.character_id
+        """, (name, air_date, episode_code, character_id))
+
+
+def get_rickandmorty_data(data_type):
+    output_data = []
+    row_data = requests.get(f'https://rickandmortyapi.com/api/{data_type}').json()
+    output_data.extend(row_data['results'])
+    urls = [f'https://rickandmortyapi.com/api/{data_type}?page={url_}' for url_ in
+            range(2, (row_data['info']['pages'] + 1))]
+    for page in urls:
+        page_info = requests.get(page).json()
+        output_data.extend(page_info['results'])
+    return output_data
+
+
 def main():
     connection = psycopg2.connect(
         host=host,
@@ -12,91 +111,25 @@ def main():
         port=port
     )
     with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS characters(
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(100) NOT NULL,
-                status VARCHAR(100),
-                species VARCHAR(100),
-                gender VARCHAR(100)
-            );"""
-        )
-
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS locations(
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(100) NOT NULL,
-                type VARCHAR(100),
-                dimension VARCHAR(100)
-            );"""
-        )
-
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE IF NOT EXISTS episodes (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(100) NOT NULL,
-                air_date DATE,
-                episode VARCHAR(100)
-            );"""
-        )
-    with connection.cursor() as cursor:
-        cursor.execute("""
-            CREATE TABLE IF NOT EXISTS character_episode (
-                character_id INTEGER,
-                episode_id INTEGER
-            )
-        """)
+        create_characters_table(cursor)
+        create_locations_table(cursor)
+        create_episodes_table(cursor)
 
     connection.commit()
 
-    character_data = requests.get('https://rickandmortyapi.com/api/character').json()
-    location_data = requests.get('https://rickandmortyapi.com/api/location').json()
-    episodes_data = requests.get('https://rickandmortyapi.com/api/episode').json()
+    character_data = get_rickandmorty_data('character')
+    location_data = get_rickandmorty_data('location')
+    episodes_data = get_rickandmorty_data('episode')
 
     with connection.cursor() as cursor:
-        for character in character_data["results"]:
-            name = character['name']
-            status = character['status']
-            species = character['species']
-            gender = character['gender']
-
-            cursor.execute("""
-                INSERT INTO characters (name, status, species, gender) 
-                VALUES (%s, %s, %s, %s) 
-                ON CONFLICT (name) DO UPDATE 
-                SET status = EXCLUDED.status, species = EXCLUDED.species, gender = EXCLUDED.gender
-            """, (name, status, species, gender))
+        insert_characters(cursor, character_data)
 
     with connection.cursor() as cursor:
-        for location in location_data["results"]:
-            name = location['name']
-            type = location['type']
-            dimension = location['dimension']
-
-            cursor.execute("""
-                INSERT INTO locations (name, type, dimension) 
-                VALUES (%s, %s, %s) 
-                ON CONFLICT (name) DO UPDATE 
-                SET type = EXCLUDED.type, dimension = EXCLUDED.dimension
-            """, (name, type, dimension))
+        insert_locations(cursor, location_data)
 
     with connection.cursor() as cursor:
-        for episode in episodes_data["results"]:
-            name = episode['name']
-            air_date = episode['air_date']
-            episode = episode['episode']
-
-
-            cursor.execute("""
-                INSERT INTO episodes (name, air_date, episode)
-                VALUES (%s, %s, %s)
-                ON CONFLICT (name) DO UPDATE
-                SET air_date = EXCLUDED.air_date, episode = EXCLUDED.episode
-                RETURNING id
-            """, (name, air_date, episode))
-
+        insert_episodes(cursor, episodes_data)
+    print('All data updated')
 
     connection.commit()
     connection.close()
---
+++ ram_top.py
@@ -0,0 +1,67 @@
+import argparse
+import psycopg2
+
+from config import host, user, password, db, port
+
+
+# connecting to database and getting data
+def connect_and_data_database():
+    with psycopg2.connect(host=host, user=user, password=password, database=db, port=port) as connection:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM episodes;")
+            all_episodes = cursor.fetchall()
+
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT * FROM characters;")
+            all_characters = cursor.fetchall()
+
+    return all_episodes, all_characters
+
+
+# a function to get a list of characters corresponding to the conditions of the script
+def get_most_characters(all_episodes, all_characters, value):
+    min_episodes = int(len(all_episodes) * value / 100)
+    matching_characters = []
+    for character in all_characters:
+        if len(character[-1]) > min_episodes:
+            matching_characters.append(character)
+
+    top_characters = sorted(matching_characters, key=lambda x: len(x[-1]), reverse=True)
+    return top_characters
+
+
+# a function to get a list of episodes that match the conditions of the script
+
+def get_most_episodes(all_episodes, value):
+    matching_episodes = []
+    for episode in all_episodes:
+        if value in episode[-1]:
+            matching_episodes.append(episode)
+    top_episodes = sorted(matching_episodes, key=lambda x: x[1])
+    return top_episodes
+
+
+def main():
+    # Handling command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--value', type=int, choices=range(1, 101),
+                        help='Integer must be between 1 and 100')
+    parser.add_argument('-t', '--type', type=str, choices=['characters', 'episodes'],
+                        help='The word must be  characters or  episodes')
+    args = parser.parse_args()
+
+    if args.type == 'characters':
+        all_episodes, all_characters = connect_and_data_database()
+        result = get_most_characters(all_episodes, all_characters, args.value)
+        for row in result[:10]:
+            print(row[2], len(row[-1]))
+    elif args.type == 'episodes':
+        all_episodes, all_characters = connect_and_data_database()
+
+        result = get_most_episodes(all_episodes, args.value)
+        for elem in result[:10]:
+            print(elem[1])
+
+
+if __name__ == '__main__':
+    main()
